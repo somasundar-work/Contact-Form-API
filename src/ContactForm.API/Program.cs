22d2cbfb8eb97f52c925376150c315eb6094e163
Somasundar <somasundar.work@outlook.com>
2025-05-06 00:07:12 +0530

    Add health check services and endpoints to the API

--- src/ContactForm.API/Program.cs
+++ src/ContactForm.API/Program.cs
@@ -3,19 +3,25 @@ using FastEndpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints();
+builder.Services.AddHealthChecks();
+builder.Services.AddEndpointsApiExplorer();
 
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Contact Form API is running!");
+
+app.MapHealthChecks("/health");
 
 app.UseFastEndpoints();
 
